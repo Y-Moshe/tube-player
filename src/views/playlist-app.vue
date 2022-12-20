6d811f2734f58a31d935b70f9c98262459ca331c
Y-Moshe <moshe.nehemiah@gmail.com>
2022-12-20 15:37:09 +0200

    render videos

--- src/views/playlist-app.vue
+++ src/views/playlist-app.vue
@@ -1,22 +1,38 @@
 <template>
-  <main>
-    PLAYLIST
-    <videos-list :videos="videos"/>
+  <main class="playlist-app">
+    <search-bar v-model="querySearch" @change="handleSearch" />
+
+    <div class="videos-container">
+      <videos-list :videos="videos"/>
+    </div>
   </main>
 </template>
 
 <script>
 import videosList from '../components/videos-list.vue';
+import searchBar from '../components/search-bar.vue'
+import { ytService} from '../services/youtube.service'
+
 export default {
   name: 'playlist-app',
+  components: {
+    videosList,
+    searchBar
+  },
   data() {
     return {
-
+      querySearch: '',
+      videos: []
     }
   },
-
-  components: {
-    videosList
+  methods: {
+    async handleSearch({ target }) {
+      const query = target.value
+      console.log('query', query)
+      const videos = await ytService.getYouTubeVideos(query)
+      console.log('videos', videos)
+      this.videos = videos
+    }
   }
 }
 </script>
