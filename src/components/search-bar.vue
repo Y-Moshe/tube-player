1c72422f7ec2e9f8c504a0a229e2e940c0c75128
Y-Moshe <moshe.nehemiah@gmail.com>
2022-12-20 14:51:02 +0200

    search-bar cmp

---
+++ src/components/search-bar.vue
@@ -0,0 +1,16 @@
+<template>
+  <section class="search-bar">
+    <form @submit.prevent="handleSubmit">
+      <input type="search" placeholder="Search on YouTube..">
+      <button type="submit">Search</button>
+    </form>
+  </section>
+</template>
+
+<script>
+export default {
+  props: {
+    querySearch: String
+  }
+}
+</script>
